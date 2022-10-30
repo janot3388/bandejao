340ffa9b2059f72368e00f1d892a117b671aa26a
janot3388 <janotbrillard3388@gmail.com>
2022-10-29 22:35:51 -0300

    t2

--- bandejao copy/control.py
+++ bandejao copy/control.py
@@ -11,13 +11,13 @@ class QRCodee:           #classe geradora do QRcode
         self.cpf = cpf
         self.nome = nome
         self.dre = dre
-        self.qrdata = ("http.site/lerQR/" + str(self.cpf) + "+" + str(self.dre) + "+" + self.nome + "/" )
+        self.qrdata = ("http://127.0.0.1:5000/lerQR/" + str(self.cpf) + "+" + str(self.dre) + "+" + self.nome + "/" )
 
     def qrmake(self):
         img = qrcode.make(self.qrdata)
         type(img)
         img.save("bandejao copy/static/QRc.png")
 
-'''s'''
+
 p1 = QRCodee(144934888400,"Janot_de_Carvalho",122072511)
 p1.qrmake()
