6f4f8d4c2f4d9f2cc172294a59c806acd0b86ed3
janot3388 <janotbrillard3388gmail>
2022-10-30 13:33:46 -0300

    arr

--- bandejao-main/bandejao copy/control.py
+++ bandejao-main/bandejao copy/control.py
@@ -7,16 +7,13 @@ import qrcode, time, datetime
 
 
 
-################################################### COMENTADO PQ N TO USANDO A DB PRA TESTAR
-#from db import user      # Importa dados do usuario
-#nome=user.nome, cpf=user.cpf, dre=user.dre
+########################## COMENTAR CASO TESTAR DIRETO SEM USAR DB !!
+from db import user      # Importa dados do usuario
+nome=user.nome, cpf=user.cpf, dre=user.dre
 
 
 
-
-
-
-class QRCodeOBJ:           # Classe geradora do QRcode
+class QRCodeOBJ:                     # Classe geradora do QRcode
     def __init__(self,cpf,nome,dre):
         self.cpf = cpf
         self.nome = nome
@@ -26,27 +23,27 @@ class QRCodeOBJ:           # Classe geradora do QRcode
     def qrmake(self):
         img = qrcode.make(self.qrdata)
         type(img)
-        img.save("bandejao copy/static/QRc.png")
+        img.save("bandejao-main/bandejao copy/static/QRc.png")
+
 
 ###TESTE DO OBJ
-# p1 = QRCodeOBJ(144934888400,"Janot_de_Carvalho",122072511)
-# p1.qrmake()
+p1 = QRCodeOBJ(cpf,nome,dre)
+p1.qrmake()
 
 
-class Fila:
+class Fila:                       # Classe geradora da fila de espera
     def __init__(self):
-
-
         pass
+
 
     def espera(self,h, m):        # Contador que simula tempo de espera da fila
         segundos = h * 60 + m     # OBS: UMA HORA É REPRESENTADA POR UM SEGUNDO
  
         while segundos > 0:
- 
             # Timer representa tempo faltando, para exportar pro processamento
             timer = datetime.timedelta(seconds = segundos)
-        
+
+            # Espera um segundo enquanto conta um a menos na variável
             time.sleep(1)
             segundos -= 1
  
